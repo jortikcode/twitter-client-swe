031ed5843d956a0215c9b28d623eed7652624c65
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-10-23 16:40:47 +0200

    updated homepage style path

--- frontend/src/components/ContenutoHomepage.js
+++ frontend/src/components/ContenutoHomepage.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import StileHomepage from '../StileHomepage.module.css';
+import homepageStyle from './ContenutoHomepage.module.css';
 
-function ContenutoHomepage () {
+function ContenutoHomepage () { 
     return (
         <React.Fragment>
-            <h1 className={StileHomepage.Titolo}>La Ghigliottina</h1>
+            <h1 className={homepageStyle.Titolo}>La Ghigliottina</h1>
             <br/>
-            <p className={StileHomepage.Testo}>Benvenuto! Sei un grande fan de "L'Eredità", il quiz più longevo della televisione italiana? Sei nel posto giusto! Gioca con migliaia di utenti da tutta Italia e scopri chi è riuscito a indovinare la parola nascosta durante l'ultima sfida: "La Ghigliottina"!</p>
+            <p className={homepageStyle.Testo}>Benvenuto! Sei un grande fan de "L'Eredità", il quiz più longevo della televisione italiana? Sei nel posto giusto! Gioca con migliaia di utenti da tutta Italia e scopri chi è riuscito a indovinare la parola nascosta durante l'ultima sfida: "La Ghigliottina"!</p>
         </React.Fragment>
     )
 }
