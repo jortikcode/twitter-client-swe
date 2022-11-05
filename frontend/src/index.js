12b39220e558adb3b25a3daa887784b3d9d8e150
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-05 16:02:07 +0100

    refactor of store creation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { setupStore } from './store/store'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,7 +11,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={ store }>    
+  <Provider store={ setupStore() }>    
       <App />
   </Provider>
 );
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { reducer } from '../reducers/index'
 
-export const store = configureStore( { reducer } );
-// export const store = createStore(reducer, applyMiddleware(thunk));
+export const setupStore = (preloadedState = {}) => {
+    return configureStore({
+      reducer,
+      preloadedState
+    });
+}
