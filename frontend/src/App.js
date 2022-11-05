b5cbc8bf42dd4eb56a0fb11c76f12cc324c2ce1a
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-05 15:38:53 +0100

    moved browser to App component for tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './screens/Homepage'
 import Navbar from './components/Navbar'
 import SearchForm from './screens/SearchForm';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/search' element={<SearchForm />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 
@@ -12,10 +11,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={ store }>
-    <BrowserRouter>
+  <Provider store={ store }>    
       <App />
-    </BrowserRouter>
   </Provider>
 );
 
