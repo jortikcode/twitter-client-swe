fb5a331d035f86aef4b150f46c7eab1b1560a895
HiImAlex-hub <alexandru.nicolescu06@gmail.com>
2022-11-06 18:24:16 +0100

    Generalizzato tramite uso di middleware

--- backend/twitterfetch.js
+++ backend/twitterfetch.js
@@ -1,9 +1,8 @@
 import { Client } from "twitter-api-sdk";
-import Express from "express";
+import Express, { response } from "express";
 const router = Express.Router();
 import dotenv from "dotenv";
 import * as cr from "./utils/customResponse.js";
-import fetch from 'node-fetch';
 
 dotenv.config();
 
@@ -12,7 +11,8 @@ const oneWeekTimestamp = 604800000;
 /* Definisco il client in application mode */
 const client = new Client(process.env.bearertoken);
 
-router.get("/search", async (req, res) => {
+/* Middleware che controlla i dati per la successiva richiesta */
+const prepareDataInput = (req, res, next) => {
   try {
     let params = req.query;
     let today = Date.now();
@@ -65,11 +65,21 @@ router.get("/search", async (req, res) => {
       "name",
       "place_type",
     ];
+    req.params = { ...params };
+    next();
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+};
+
+router.get("/search", prepareDataInput, async (req, res) => {
+  try {
+    const params = req.params;
     const response = await client.tweets.tweetsRecentSearch(params);
     if (response.meta.result_count == 0)
       // Non sono stati trovati risultati
-      res.status(200).json({no_matches: true});
-    else{
+      res.status(404).json({ no_matches: true });
+    else {
       // Array contenente gli id degli autori dei tweet ricevuti dalla richiesta
       let authorsId = [];
       // Array contenete i tipi dei tweet: TWEET, RETWEET, REPLY
@@ -100,81 +110,38 @@ router.get("/search", async (req, res) => {
   }
 });
 
-// TODO: generalizzare il codice duplicato che c'e' tra gli endpoint '/search' e '/tweets'
+/* Middleware per prendere l'id dell'utente dal sul username */
+const getUserID = async (req, res, next) => {
+  try {
+    const { username } = req.query;
+    if (!username) {
+      throw "Username mancante";
+    }
+    // Username dell'utente di cui ci interessano i tweets
+    //Interrogazione della API all'URL https://api.twitter.com/2/users/by/username/:username
+    const response = await client.users.findUserByUsername(username);
+    if (response?.errors) {
+      throw "Nessun utente trovato";
+    }
+    // id dell'utente risultante dalla richiesta
+    const { id } = response.data;
+    req.userID = id;
+    next();
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
 
 /* GET /api/tweets?username=string&start_time=string&end_time=string&max_results=int
 }*/
-router.get("/tweets", async (req, res) => {
-  try{
-    const { username } = req.query;
-    const start_time = req.query?.start_time;
-    const end_time = req.query?.end_time;
-    const today = Date.now();
-    if (! username)
-      // Richiesta invalida
-      return res.status(200).send({ error: "Richiesta non valida" });
-    if (start_time && end_time) {
-      if (Date.parse(start_time) > Date.parse(end_time)) {
-        throw "Date non valide, la data di inizio è dopo della data di fine";
-      }
-    }
-
-    if (start_time) {
-      const start = Date.parse(start_time);
-      if (start < today - oneWeekTimestamp) {
-        throw "Data di inizio non valida, le date valide sono solo quelle nell'arco dell'ultima settimana";
-      }
-    }
-
-    if (end_time) {
-      const end = Date.parse(end_time);
-      if (end > today) {
-        throw "Data di fine non valida, le date valide sono solo quelle nell'arco dell'ultima settimana";
-      }
-    }
-    let params = {};
-    params["tweet.fields"] = [
-      "attachments",
-      "author_id",
-      "entities",
-      "geo",
-      "id",
-      "text",
-      "created_at",
-    ];
-    params["expansions"] = [
-      "attachments.media_keys",
-      "entities.mentions.username",
-      "geo.place_id",
-      "in_reply_to_user_id",
-      "referenced_tweets.id",
-      "referenced_tweets.id.author_id",
-    ];
-    params["media.fields"] = ["preview_image_url", "type", "url"];
-    params["place.fields"] = [
-      "contained_within",
-      "country",
-      "country_code",
-      "full_name",
-      "geo",
-      "id",
-      "name",
-      "place_type",
-    ];
-    // Username dell'utente di cui ci interessano i tweets 
-    //Interrogazione della API all'URL https://api.twitter.com/2/users/by/username/:username
-    let response = await client.users.findUserByUsername(username);
-
-    if (response?.errors)
-      // Richiesta invalida, utente non trovato o invalido
-      return res.status(200).send({ error: "Utente invalido" });
-
-    // id dell'utente risultante dalla richiesta
-    const { id } = response.data;
+router.get("/tweets", prepareDataInput, getUserID, async (req, res) => {
+  try {
+    const id = req.userID;
+    const params = req.params;
     response = await client.tweets.usersIdTweets(id, params);
     if (response.meta.result_count == 0)
       // Non sono stati trovati risultati
-      return res.status(200).json({no_matches: true});
+      return res.status(200).json({ no_matches: true });
     // Array contenete i tipi dei tweet: TWEET, RETWEET, REPLY
     let types = [];
     const { payload } = cr.default.searchSuccess(
@@ -196,10 +163,9 @@ router.get("/tweets", async (req, res) => {
       types
     );
     return res.status(200).json(payload);
+  } catch (error) {
+    return res.status(500).send({ errore: "Errore HTTP " + error });
   }
-  catch(error){
-    return res.status(500).send({errore: "Errore HTTP " + error});
-  }
-})
+});
 
 export default router;
