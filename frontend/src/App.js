a220417e707e0ce5549901fa37d0cc3ac3405782
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-07 21:45:03 +0100

    fixed component reference

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Homepage />} />
-          <Route path='/search' element={<SearchForm />} />
+          <Route path='/search' element={<SearchPage />} />
         </Routes>
       </BrowserRouter>
     </div>
