1278749965180ccceacc5a1e6d8f02d9ac2249fb
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-12 20:07:41 +0100

    tweet list component added (towards tabs)

---
+++ frontend/src/components/TweetList.js
@@ -0,0 +1,32 @@
+import Tweet from "./Tweet";
+const TweetList = ({ textTweets, places, users, types, creationDates }) => {
+    return (
+        <>
+        { ((textTweets.length > 0) && (
+            <div className="pt-8 flex gap-y-10 flex-col justify-center items-center md:w-4/6 w-4/5 dark:text-white">
+                {textTweets.map((tweet, index) =>
+                {
+                    let placeName = "";
+                    if (places.find(place_info => place_info.index === index))
+                        for (const place_info of places){
+                            if (place_info.index === index){
+                                placeName = place_info.name;
+                                break;
+                            }
+                        }
+                    return (<Tweet
+                        placeName={placeName}
+                        key={index}
+                        name={users[index].name} 
+                        username={users[index].username} 
+                        pfpUrl={users[index].pfpUrl}
+                        type={types[index]}
+                        date={new Date(creationDates[index]).toDateString()}
+                        text={tweet.text} />)})}
+            </div> 
+        )) }
+        </>
+    );
+}
+
+export default TweetList;
