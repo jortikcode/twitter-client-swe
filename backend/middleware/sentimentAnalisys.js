ac25beea760148a4e7c60969ff26cbd9ee6322fe
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-14 16:25:34 +0100

    fixed searchSentimentAnalysis

--- backend/middleware/sentimentAnalisys.js
+++ backend/middleware/sentimentAnalisys.js
@@ -1,15 +1,17 @@
 import { default as sentiment } from "multilang-sentiment";
 import { languages } from "../utils/constants.js";
 
+// Lingua predefinita: la lingua del tweet non e' supportata da multilang-sentiment, essa viene forzata ad essere "en"
 const defaultLang = "en";
 
+// Middleware per la sentiment analysis dei singoli tweet
 export const tweetSentimentAnalysis = (req, res, next) => {
   let result = [];
   const tweets = { ...req.payload.textTweets };
   const len = req.payload.textTweets.length;
   for (let i = 0; i < len; i += 1) {
     let lang = defaultLang;
-    if (languages.includes(payload.tweets[i].lang)) {
+    if (languages.includes(req.payload.textTweets[i].lang)) {
       lang = tweets[i].lang;
     }
     const sent = sentiment(tweets[i].text, lang);
@@ -26,8 +28,9 @@ export const tweetSentimentAnalysis = (req, res, next) => {
   });
 };
 
+// Middleware per la sentiment analysis complessiva della ricerca
 export const searchSentimentAnalysis = (req, res, next) => {
-  const tweets = { ...req.payload.textTweets };
+  const tweets = req.payload.textTweets;
   const len = tweets?.length;
   if (len > 0) {
     let sentimentAnalysis = {
@@ -39,7 +42,11 @@ export const searchSentimentAnalysis = (req, res, next) => {
     };
     for (let i = 0; i < len; i++) {
       try {
-        let partial = sentiment(tweets[i].text, tweets[i].lang || defaultLang);
+        let lang = defaultLang;
+        if (languages.includes(tweets[i].lang)) {
+          lang = tweets[i].lang;
+        }
+        let partial = sentiment(tweets[i].text, lang);
         sentimentAnalysis.comparative += partial.comparative;
         sentimentAnalysis.score += partial.score;
 
@@ -49,14 +56,12 @@ export const searchSentimentAnalysis = (req, res, next) => {
           partial.tokens.length -
           (sentimentAnalysis.positives + sentimentAnalysis.negatives);
       } catch (error) {
-        //console.log(error)
+        throw new Error("Errore della sentiment: " + error);
       }
     }
-
-    sentimentAnalysis.score /= req.data.statuses.length;
-    sentimentAnalysis.comparative /= req.data.statuses.length;
-
-    req.payload.sentimentAnalysis = sentimentAnalysis;
+    sentimentAnalysis.score /= len;
+    sentimentAnalysis.comparative /= len;
+    req.payload.searchSentimentAnalysis = sentimentAnalysis;
     next();
   } else {
     next();
