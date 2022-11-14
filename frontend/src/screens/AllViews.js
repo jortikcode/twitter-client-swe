245446d42d92238048865297d20cc03e4becf34a
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-14 16:27:37 +0100

    new pie chart of search sentiment

--- frontend/src/screens/AllViews.js
+++ frontend/src/screens/AllViews.js
@@ -11,7 +11,8 @@ const AllViews = () => {
         creationDates, 
         types, 
         places, 
-        sentiments } = useSelector(state => state.tweets);
+        sentiments,
+        searchSentiment } = useSelector(state => state.tweets);
     return (
         <div className="flex w-full flex-col dark:bg-gray-900 items-center gap-y-8">
             {textTweets.length > 0 && (
@@ -23,8 +24,9 @@ const AllViews = () => {
                 users={users}
                 places={places}
                 /> 
-                <PieChart sentAnalysis = {sentiments} />
-                <BarChart creationDates = {creationDates} />
+                <PieChart title="Analisi dei tweet" sentAnalysis = {sentiments} />
+                <PieChart title="Analisi delle parole dei risultati" sentiments = {[searchSentiment.negatives, searchSentiment.positives, searchSentiment.neutrals]} />
+                <BarChart title="Tweet nel tempo" creationDates = {creationDates} />
                 { places.length > 0 &&
                 (
                 <div className="w-full md:p-8 p-3 dark:bg-gray-900">
