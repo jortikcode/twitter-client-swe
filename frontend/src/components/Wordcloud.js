321f68823244bec05a0113ae0b7a25b21e054a60
Riccardo Fava <rickyfava01@gmail.com>
2022-11-22 22:39:08 +0100

    Installazione di react-d3-cloud e creazione del componente Wordcloud

---
+++ frontend/src/components/Wordcloud.js
@@ -0,0 +1,43 @@
+import React from "react";
+import WordCloud from "react-d3-cloud";
+
+
+const Wordcloud = (props) => {
+    //dati di prova
+    const data = [
+        { text: "Hey", value: 1000 },
+        { text: "lol", value: 200 },
+        { text: "first impression", value: 1000 },
+        { text: "very cool", value: 1000000 },
+        { text: "duck", value: 10 },
+        { text: "very cool", value: 1000 },
+        { text: "very cool", value: 1000 },
+        { text: "very cool", value: 1000 },
+        { text: "very cool", value: 1000 },
+        { text: "very cool", value: 1000 },
+        { text: "very cool", value: 1000 },
+    ];
+
+    //const data = props.words (array ritornato da backend)
+
+
+    //TODO: cambiare fontsize per adattarla ai valori dei dati ritornati da backend
+    const fontSizeMapper = (word) => Math.log2(word.value) * 5;
+
+
+    return (
+        <div style={{ margin: "250px" }}>
+            <WordCloud
+                data={data}
+                fontSizeMapper={fontSizeMapper}
+                width={500}
+                height={500}
+                padding={3}
+                font="impact"
+            />
+        </div>
+    )
+}
+
+
+export default Wordcloud
