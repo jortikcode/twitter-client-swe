ea8bebbad55ab02cde1ba46d835d8315bc645c4b
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-22 18:46:11 +0100

    bit more elegant & optimal wordcloudInfo

--- backend/middleware/wordcloudInfo.js
+++ backend/middleware/wordcloudInfo.js
@@ -1,119 +1,54 @@
-import { languages } from "../utils/constants.js";
-
-
-// npm install keyword-extractor
+import { extractorSupportedLanguages,
+         maxWordsPerWordcloud } from "../utils/constants.js";
 import keyword_extractor from "keyword-extractor";
 
 
-// Lingua predefinita: la lingua del tweet non e' supportata da multilang-sentiment, essa viene forzata ad essere "en"
-const defaultLang = "en";
+// Lingua predefinita per effettuare l'estrazione delle keyword
+const defaultLang = "english";
 
 // Middleware per la wordcloud dei tweet
 export const getWordcloudInfo = (req, res, next) => {
-  let result = [];
-  const tweets = { ...req.payload.textTweets };
+  const tweets = req.payload.textTweets;
   const len = req.payload.textTweets.length;
 
   let words = [];
 
   for (let i = 0; i < len; i += 1) {
-    // identifica la lingua del tweet
-    let lang = defaultLang;
-    if (languages.includes(req.payload.textTweets[i].lang)) {
-      lang = tweets[i].lang;
-    }
-
-    let lang1 = lang;
-    switch(lang) {
-      case "it":
-        lang1 = "italian";
-        break;
-      case "es":
-        lang1 = "spanish";
-        break;
-      case "de":
-        lang1 = "german";
-      break;
-      case "fr":
-        lang1 = "french";
-      break;
-      case "pl":
-        lang1 = "polish";
-      break;
-      case "nl":
-        lang1 = "dutch";
-      break;
-      case "ro":
-        lang1 = "romanian";
-      break;
-      case "ru":
-        lang1 = "russian";
-      break;
-      case "pt":
-        lang1 = "portuguese";
-      break;
-      case "sv":
-        lang1 = "swedish";
-      break;
-      case "ar":
-        lang1 = "arabic";
-      break;
-      case "fa":
-        lang1 = "persian";
-      break;
-      default:
-        lang1 = "english";
-    }
-
+    // Ci ricaviamo la lingua del tweet per esteso, se non e' supportata, viene settata ad una lingua di default
+    const language = extractorSupportedLanguages[tweets[i].lang] || defaultLang;
     // funzione che estrae le parole chiave di un testo
     const extraction_result =
     keyword_extractor.extract(tweets[i].text,{
-        language:lang1,
+        language,
         remove_digits: true,
         return_changed_case:true,
         remove_duplicates: false
-
     });
     // inserisco tutte le parole chiave in un array
     words = words.concat(extraction_result);
   }
 
-  // rimuovo le parole di lunghezza 1 (solitamente simboli/emoji)
-  let words1 = [];
-  for (let word of words) {
-    if (word.length > 1) {
-      words1.push({text: word, value: 0});
-    }
-  }
-  words = words1;
+  // Rimozione delle parole di lunghezza 1 (solitamente simboli/emoji)
+  const significantWords = words.flatMap(word => word.length === 1 ? [] : [word])
 
-  // conto per ogni parola chiave, quante volte compare nell'array
-  const count = {};
-  for (const element of words) {
-    if (count[element.text]) {
-      count[element.text] += 1;
-    } else {
-      count[element.text] = 1;
-    }
-  }
-
-  // trasformo l'array in una lista di oggetti di forma: {text: "testo", value: valore}
-  const arr = [];
-  for (const key in count) {
-    arr.push({text: key, value: count[key]});
-  }
-  
-  // ordino l'array
-  arr.sort((a,b) => b.value - a.value);
-  //console.log(arr);
-
-  /*  
-      per passare solo una parte dell'array
-      arr = arr.slice(0, fine); 
+  /* 
+  Conteggio per ogni parola chiave, quante volte compare e restituiamo il risultato in un 
+  array che ha elementi del tipo: parola: counter
   */
+  const wordCounts = significantWords.reduce((infoAccumulator, word) => {
+    infoAccumulator[word] = ++infoAccumulator[word] || 1;
+    return infoAccumulator;
+  }, []);
 
-  // restituisco l'array
-  req.payload.wordcloudInfo = arr;
+  let wordcloudInfoArray = [];
+  // Trasformiamo l'array in un array di oggetti di forma: {text: "testo", value: valore}
+  for (const word in wordCounts) {
+    wordcloudInfoArray.push({text: word, value: wordCounts[word]});
+  }
 
+  // Ordinamento dell'array e rimozione delle keyword con valori non significativi
+  wordcloudInfoArray = wordcloudInfoArray.sort((a,b) => b.value - a.value).slice(0, maxWordsPerWordcloud);
+  console.log(wordcloudInfoArray, wordcloudInfoArray.length);
+  req.payload.wordcloudInfo = wordcloudInfoArray;
   next();
 }
