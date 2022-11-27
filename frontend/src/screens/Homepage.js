252e318ef0018be503c1eeb6869a4c4b68d75a94
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-27 18:52:33 +0100

    clear tweets at first render of page

--- frontend/src/screens/Homepage.js
+++ frontend/src/screens/Homepage.js
@@ -1,5 +1,13 @@
 import ContenutoHomepage from "../components/ContenutoHomepage";
+import { useEffect } from "react";
+
+import { useDispatch } from "react-redux";
+import { clearTweets } from "../actions/customActions";
 const Homepage = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(clearTweets())
+    }, [dispatch])
     return (
         <>
             <ContenutoHomepage />
