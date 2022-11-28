95c2fef46c2b12de6bb018e9d148108aaf5df4d5
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2022-11-28 20:15:32 +0100

    refactor: renamed customActions to tweets

--- frontend/src/screens/Homepage.js
+++ frontend/src/screens/Homepage.js
@@ -2,7 +2,7 @@ import ContenutoHomepage from "../components/ContenutoHomepage";
 import { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
-import { clearTweets } from "../actions/customActions";
+import { clearTweets } from "../actions/tweets";
 const Homepage = () => {
     const dispatch = useDispatch();
     useEffect(() => {
