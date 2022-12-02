7ccc703fba728ec4a8f7ac17d64f33616ead5077
alexandrunicolescu <alexandru.nicolescu@studio.unibo.it>
2022-12-02 20:55:38 +0100

    ranking done

--- backend/controllers/fantacitorio.js
+++ backend/controllers/fantacitorio.js
@@ -12,7 +12,10 @@ export const ranking = async (req, res) => {
     );
     const rows = getValidRows(tweets.data);
     const points = getPointsfromText(rows);
-    const sortedPoints = points.sort((a, b) => { return b.points - a.points})
+    const summedPoints = getSummedPoints(points);
+    const sortedPoints = summedPoints.sort((a, b) => {
+      return b.points - a.points;
+    });
     return res.status(200).send(sortedPoints);
   } catch (error) {
     return res.status(500).send({ error: error.message });
@@ -32,6 +35,52 @@ export const weeklyPoints = async (req, res) => {
   }
 };
 
+/* prende una lista di oggetti nomePoilitico, punti e somma i punti gruppati per nomePolitico */
+function getSummedPoints(points) {
+  const surnames = [];
+  const fullNames = [];
+  for (const entry of points) {
+    /* prendo solamente quelli con solo il cognome o con il cognome di 2 parole */
+    if (
+      entry.politic.split(/\s+/).length == 1 ||
+      entry.politic.match(/^.. /g)
+    ) {
+      surnames.push(entry.politic);
+    } else {
+      fullNames.push(entry.politic);
+    }
+  }
+  for (const element of points) {
+    if (surnames.includes(element.politic)) {
+      console.log(`Primo if: ${element.politic}`);
+      for (const fullName of fullNames) {
+        if (fullName.includes(element.politic)) {
+          console.log(`Prima: ${element.politic} | ${fullName}`);
+          element.politic = fullName;
+          console.log(`Dopo: ${element.politic} | ${fullName}`);
+        }
+      }
+    }
+  }
+  const summedPoints = [];
+  for (const element of points) {
+    if (element.politic in summedPoints) {
+      summedPoints[element.politic] += element.points;
+    } else {
+      summedPoints[element.politic] = element.points;
+    }
+  }
+  const obj = { ...summedPoints };
+  const output = [];
+  for (const key of Object.keys(obj)) {
+    const row = {};
+    row.politic = key;
+    row.points = obj[key];
+    output.push(row);
+  }
+  return output;
+}
+
 /* prende una lista di righe di punteggio e ritorna una lista di oggetti nomePolitico, punti */
 function getPointsfromText(rows) {
   if (!rows) {
@@ -42,33 +91,27 @@ function getPointsfromText(rows) {
     list[i] = {};
     let politic;
     let points;
-    let splitted;
-    if (rows[i].trim().split(/\s+/).length == 3) {
-      splitted = rows[i].trim().split(/\s+/);
-      for (const word of splitted) {
-        if (word.match(/(\d+)/)) {
-          points = parseInt(word);
-        } else if (!word.match(/PUNTI|punti/)) {
-          politic = word;
-        }
-      }
-    } else {
-      if (rows[i].includes(" PER ")) {
-        splitted = rows[i].split(" PER ");
-      }
-      if (rows[i].includes(" - ")) {
-        splitted = rows[i].split(" - ");
-      }
-      if (rows[i].includes(" A ")) {
-        splitted = rows[i].split(" A ");
-      }
-      splitted[0] = splitted[0].replace(/O/g, 0);
-      points = parseInt(splitted[0].match(/(\d+)/)[0]);
-      if (splitted[0].includes("MALUS")) {
-        points *= -1;
-      }
-      politic = splitted[1];
+    if (rows[i].match(/OO/)) {
+      rows[i] = rows[i].replace(/OO/, "00");
     }
+    const words = rows[i].trim().split(" ");
+    let j = 0;
+    let prev;
+    do {
+      prev = words[j];
+      j += 1;
+    } while (words[j] != "PUNTI" && words[j] != "punti");
+    points = parseInt(prev);
+    if (rows[i].includes("MALUS")) {
+      points *= -1;
+    }
+    politic = rows[i]
+      .replace(
+        /(\d+)|(PUNTI|punti)|( - )|( A )|( PER )|(ALTRI )|(\+)|(\(TOT\.)|(\))|(MALUS DI )/g,
+        ""
+      )
+      .trim();
+
     list[i].politic = politic;
     list[i].points = points;
   }
@@ -91,7 +134,7 @@ function getValidRows(tweets) {
         (row.includes(" - ") ||
           row.includes(" PER ") ||
           row.includes(" A ") ||
-          row.trim().split(/\s+/).length == 3) &&
+          row.trim().split(/\s+/).length <= 4) &&
         !(row.includes(":") || row.includes("NON "))
       ) {
         /* vado a considerare solo le righe di punteggio */
